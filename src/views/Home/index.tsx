7ca00f01fedc7dd9a5d3ed1bc5bf74eb3d559157
Micael-Macedo <micaelm2009@hotmail.com>
2026-01-29 11:22:38 -0300

    feat: add base home content

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,59 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
+
+import { style } from "./style";
+
+import Header from "@/shared/Header";
+import { IOrcamento } from "@/interfaces/orcamento";
+import OrcamentoItem from "@/shared/OrcamentoItem";
+import { StatusEnum } from "@/interfaces/statusEnum";
+import Input from "@/shared/Input";
+import { MaterialIcons } from "@expo/vector-icons";
+import Button from "@/shared/Button";
 
 export default function Home() {
+    const data: IOrcamento[] = [
+        {
+            client: "Soluções Tecnológicas Beta",
+            id: "1",
+            items: [],
+            title: "Desenvolvimento de aplicativo de loja online",
+            status: StatusEnum.APROVADO,
+            discountPct: "10",
+            createdAt: "2024-06-01T10:00:00Z",
+            updatedAt: "2024-06-05T15:30:00Z",
+        },
+        {
+            client: "Soluções Tecnológicas Beta",
+            id: "2 ",
+            items: [],
+            title: "Desenvolvimento de aplicativo de loja online",
+            status: StatusEnum.RECUSADO,
+            discountPct: "10",
+            createdAt: "2024-06-01T10:00:00Z",
+            updatedAt: "2024-06-05T15:30:00Z",
+        },
+    ]
+
     return (
-        <View>
-            
+        <View style={style.container}>
+            <Header totalDrafts={1} />
+            <View style={style.page}>
+                <View style={style.filterContainer}>
+                    <View style={style.searchContainer}>
+                        <MaterialIcons name="search" size={24} />
+                        <Input placeholder="Título ou cliente" />
+                    </View>
+                    <Button iconName="filter-list" label="" type="secondary" />
+                </View>
+                <FlatList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <OrcamentoItem data={item} />
+                    )}
+                />
+            </View>
+
         </View>
     )
 };
--- src/views/Home/style.ts
+++ src/views/Home/style.ts
@@ -1,7 +1,28 @@
+import { DEFAULT_THEME } from "@/config/theme";
 import { StyleSheet } from "react-native";
 
-const style = StyleSheet.create({
+export const style = StyleSheet.create({
     container: {
+        marginTop: 40,
         flex: 1,
-    }
+        justifyContent: "flex-start"
+    },
+    page: {
+        paddingHorizontal: 20,
+        paddingVertical: 24,
+    },
+    filterContainer:{
+        flexDirection: "row",
+        gap: 8,
+    },
+    searchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: DEFAULT_THEME.BORDER_COLOR,
+        gap: 8,
+        borderRadius: 12,
+        padding: 14,
+    },
+
 })
